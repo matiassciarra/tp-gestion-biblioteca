2e29a61417abd1fb2515f26333060bb87a91a1d5
mati <matisciarra@outlook.com>
2024-06-06 22:44:08 -0300

    Autor bien hecho

--- backend/database/models/Autor.model.js
+++ backend/database/models/Autor.model.js
@@ -1,10 +1,28 @@
 import { sequelize } from "../db.js";
 import { DataTypes } from "sequelize";
 
-sequelize.define("modeloPrueba", {
+export const Autor = sequelize.define("Autor", {
+    id_autor: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
     nombre: {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    apellido: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    biografia: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    fecha_nacimiento: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+
     timestamps: false,
 });
