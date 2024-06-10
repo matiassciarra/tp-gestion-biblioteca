a5a4953de3406d8774f1d0b6c2e9deb721074fce
mati <matisciarra@outlook.com>
2024-06-10 17:37:38 -0300

    Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./components/generales/NavBar";
+import AppRouter from "./router.config";
+
+function App() {
+    return (
+        <>
+            <NavBar></NavBar>
+            <AppRouter></AppRouter>
+        </>
+    );
+}
+
+export default App;
