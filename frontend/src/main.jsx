d474f570b6461aa9841271b492dfacb52a6a2076
mati <matisciarra@outlook.com>
2024-06-10 17:50:30 -0300

    Eliminada carpeta router config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    
-        <React.StrictMode>
-            <BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter>
             <App />
-            </BrowserRouter>
-            
-        </React.StrictMode>
+        </BrowserRouter>
+    </React.StrictMode>
 );
