6108cc539a88b0a0ed525a8bd804c485b9d698e8
Franco95403 <95403@sistemas.frc.utn.edu.ar>
2024-06-11 13:53:04 -0300

    Actualizacion pagina allGeneros

--- frontend/src/pages/Genero/AllGeneros.jsx
+++ frontend/src/pages/Genero/AllGeneros.jsx
@@ -1,17 +1,62 @@
 import { useLoaderData } from "react-router-dom"
+import '../../assets/generos/AllGeneros.css'
+import Button from "react-bootstrap/esm/Button";
+import { useState } from "react";
+import { deleteGenero } from "../../service/generos";
 
 export const AllGeneros=()=>{
-    const res = useLoaderData()
-    console.log(res);
+    const initialData =useLoaderData();
+    const [generos, setGeneros] =useState(initialData)
+    
+    const [searchTerm, setSearchTerm] = useState("");
+    const handleSearch =(e) =>{
+        e.preventDefault();
+        console.log("Buscando genero:", searchTerm);
+    }
+    const handlerDelete = async (id) => {
+        try {
+          // Llama a la API para eliminar el genero
+          await deleteGenero(id);
+          // Actualiza el estado filtrando el genero eliminado
+          setGeneros(generos.filter(genero => genero.id_genero !== id));
+          
+        } catch (error) {
+          console.error("Error al eliminar el genero:", error);
+        }
+      }
     return (
-        <div className="container-fluid text-bg-secondary p-3">
+        
+        <div className="conteiner-all-generos ">
+            <div className="add-button-container mt-4">
+                <button className="btn btn-success fw-bold">Agregar Género</button>
+            </div>
+        {/*Formulario para buscar generos */}
+        <div className="search-container mt-4">
+                <form onSubmit={handleSearch}>
+                    <input 
+                       type ='text'
+                       className="form-control"
+                       placeholder="Buscar Genero"
+                       value={searchTerm}
+                       onChange={(e)=>setSearchTerm(e.target.value)}
+                        />
+                        <button type="submit" className="btn btn-primary fw-bold mt-2">Buscar</button>
+                </form>
+            </div>
             {
-                res.map(({id_genero, nombre})=>(
+                generos.map(({id_genero, nombre})=>(
                     <article className="card card-body" key={id_genero}>
-                        <p className="card-title">{nombre}</p>
+                        <h1 className="card-title text-black">{nombre}</h1>
+                        <div>
+                            <button type='button' className="btn btn-primary card-link fw-bold">Editar</button>
+                            <button type='button' onClick={() => handlerDelete(id_genero)} className="btn btn-danger  card-link fw-bold">Eliminar</button>
+                        </div>
+
                     </article>
+                    
                 ))
             }
+            
         </div>
     )
 }
--- frontend/src/service/generos.js
+++ frontend/src/service/generos.js
@@ -4,3 +4,16 @@ export const getAllGenero = async()=>{
     const res= await fetch(URL)
     return res.json()
 }
+export const deleteGenero = async (id) => {
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    const response = await fetch(URL+`/${id}`, options);
+    if (!response.ok) {
+        throw new Error('Problema con la petición Fetch: ' + response.statusText);
+    }
+    return await response.json();
+}
