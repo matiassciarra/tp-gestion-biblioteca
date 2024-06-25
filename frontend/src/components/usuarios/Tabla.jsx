2b7bbde526435cce9be331dd44bd136153226dd0
martinn-rodriguez <97526@sistemas.frc.utn.edu.ar>
2024-06-25 00:31:17 -0300

    tabla usuarios

--- frontend/src/components/usuarios/Tabla.jsx
+++ frontend/src/components/usuarios/Tabla.jsx
@@ -2,12 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { getAllUsuarios } from '../../service/usuarios';
 import { getPaises } from '../../service/paises'
 import { getTipoUsuarios } from '../../service/tipoUsuario';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+import { deleteUsuario } from '../../service/usuarios'
 
 
 const TablaUsuarios = () => {
     const [usuarios, setUsuarios] = useState([]);
     const [paisesMap, setPaisesMap] = useState({});
     const [tiposUsuarioMap, setTiposUsuarioMap] = useState({})
+
+    const handleDelete = async (id) => {
+        try {
+            await deleteUsuario(id);
+            setUsuarios(usuarios.filter(usuario => usuario.id_usuario !== id));
+        } catch (error) {
+            console.error('Error al eliminar el usuario')
+        }
+    };
 
     useEffect(() => {
 
@@ -56,15 +68,16 @@ const TablaUsuarios = () => {
     return (
         <div className="table-responsive rounded p-4">
             <h2 className='my-4 text-center'>Lista de Usuarios</h2>
-            <table className="table table-striped table-bordered">
+            <table className="table table-striped table-bordered text-center">
                 <thead className='thead-dark'>
                     <tr>
                         <th>Nombre</th>
                         <th>Apellido</th>
-                        <th>Correo</th>
                         <th>UserName</th>
+                        <th>Email</th>
                         <th>País</th>
                         <th>Tipo de Usuario</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -76,6 +89,29 @@ const TablaUsuarios = () => {
                             <td>{usuario.correo}</td>
                             <td>{paisesMap[usuario.id_pais]}</td>
                             <td>{tiposUsuarioMap[usuario.id_tipo_usuario]}</td>
+                            <td>
+                                <button
+                                    className='btn btn-info btn-sm m-1'
+                                    //onClick
+                                    title='Consultar'
+                                >
+                                    <FontAwesomeIcon icon={faSearch}/>
+                                </button>
+                                <button
+                                    className='btn btn-warning btn-sm m-1'
+                                    //onClick
+                                    title='Modificar'
+                                >
+                                    <FontAwesomeIcon icon={faEdit}/>
+                                </button>
+                                <button
+                                    className='btn btn-danger btn-sm m-1'
+                                    onClick={() => handleDelete(usuario.id_usuario)}
+                                    title='Eliminar'
+                                >
+                                    <FontAwesomeIcon icon={faTrashAlt}/>
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
